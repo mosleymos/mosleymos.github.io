3b59b03faa55faf14e71cc28a673e7ea5afd93a5
keita mori <keitamori@gmail.com>
2016-01-12 21:21:41 +0100

    Add browserify and js concatenation and minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
-var gulp  = require('gulp');
+var gulp        = require('gulp');
 var browserSync = require('browser-sync');
-var sass = require('gulp-ruby-sass');
-var reload = browserSync.reload;
+var sass        = require('gulp-ruby-sass');
+var uglify      = require('gulp-uglify');
+var concat      = require('gulp-concat');
+var transform   = require('vinyl-transform');
+var source      = require('vinyl-source-stream');
+var browserify  = require('browserify');
+var reload      = browserSync.reload;
 
 gulp.task('default', function(){
 
+});
+
+// Little help from https://egghead.io/lessons/javascript-gulp-and-browserify-initial-setup
+gulp.task('browserify', function(){
+  browserify('js/main.js')
+    .bundle()
+    .on('error', function(){
+      console.log('ca foire' +  e);
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('script', function(){
+  return gulp.src('js/main.js')
+  .pipe(concat('all.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', function(){
