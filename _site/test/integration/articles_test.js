a42527f6c2bb5815aa1eec3768882d8a4c882a6d
mori keita <keitamori@gmail.com>
2020-06-08 19:53:56 +0200

    Merge de l'independant

--- _site/test/integration/articles_test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'Article page should have the correct title' : function(test){
-      test
-         .open('http://localhost:8000/#/articles')
-	 .assert.text('h2').is('Liste des articles')
-	 .done();
-  }
-};
--- _site/test/integration/projects_test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'Projects page should display the correct title' : function(test){
-	 test
-	    .open('http://localhost:8000/#/projects')
-	    .assert.text('h2').is('Mes projets actuels')
-	    .done();
-  }
-};
